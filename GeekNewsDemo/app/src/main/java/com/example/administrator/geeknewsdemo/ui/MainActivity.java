80f73a02d7c2ada094209d5d4892c1bda5c5c635
刘亚伟 <liuyawei@thinkyun.cn>
2018-03-14 18:11:53 +0800

    空文件夹

--- GeekNewsDemo/app/src/main/java/com/example/administrator/geeknewsdemo/ui/MainActivity.java
+++ GeekNewsDemo/app/src/main/java/com/example/administrator/geeknewsdemo/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.administrator.geeknewsdemo;
+package com.example.administrator.geeknewsdemo.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.administrator.geeknewsdemo.R;
 
 public class MainActivity extends AppCompatActivity {
 
